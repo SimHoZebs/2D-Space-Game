a7c9549c223320f847b96394ee239f8a284f02a1
Sim Ho <simhoover9000@gmail.com>
2021-03-25 23:31:12 +0900

    A* Pathfinding first attempt

--- Assets/Scripts/Character/AIMovement.cs
+++ Assets/Scripts/Character/AIMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using Mirror;
 
 public class AIMovement : NetworkBehaviour
 {
+    [Header("MovementControls")]
     [SerializeField] private float aiMovementSpeed = 6f;
     [SerializeField] private float aiWalkSpeed = 3f;
     [SerializeField] private float pauseDuration = 3f;
@@ -15,10 +17,24 @@ public class AIMovement : NetworkBehaviour
     private Vector3 randomDirection;
     private Rigidbody2D rb;
 
+    [Header("A* Pathfinding")]
+    [SerializeField] private Tilemap unwalkableTilemap;
+    [SerializeField] private float possibleTargetPathRadius;
+    private List<Vector3Int> unwalkableCoords;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine("MoveRandomly");
+
+        var testNode = new Node();
+
+        if (testNode.pos == null){
+            Debug.Log("testNode pos is null");
+        }
+        else{
+            Debug.Log(testNode.pos);
+        }
     }
 
     [Server]
@@ -27,7 +43,6 @@ public class AIMovement : NetworkBehaviour
             Move(randomDirection);
         }
     }
-
 
     private IEnumerator MoveRandomly(){
         if (isLocalPlayer){ Debug.Log("Player is running this?");}
@@ -53,9 +68,122 @@ public class AIMovement : NetworkBehaviour
         }
     }
 
-    private void Move(Vector3 randomDirection){
-        rb.velocity = randomDirection*aiMovementSpeed*Time.deltaTime;
+    private void Move(Vector3 direction){
+        rb.velocity = direction*aiMovementSpeed*Time.deltaTime;
     }
 
+    [SerializeField]
+    private struct Node{
+        public float total;
+        public float distance2Start;
+        public float distanceToEnd;
+        public Vector3 pos;
+        public int prevNode;
+    }
+
+    private List<Node> openNodes = new List<Node>();
+    private List<Node> closedNodes = new List<Node>();
+    private Vector3 targetPath;
+
+    private int[] surroudingNodesOffset = {-1,0,1};
     
+    private void PathFinding(){
+        var randomAngle = Random.Range(0, 360) * Mathf.Deg2Rad;
+        targetPath = new Vector3(Mathf.Sin(randomAngle), Mathf.Cos(randomAngle));
+
+        Node startNode = CreateStartingNode(transform.position);
+        openNodes.Add(startNode);
+
+        while(true){
+            Node node = FindLowestDistance2Start();
+            openNodes.Remove(node);
+            closedNodes.Add(node);
+
+            Node[] neighbourNodes = CreateNeighbours(node);
+
+            foreach (Node neighbourNode in neighbourNodes){
+                if (unwalkableTilemap.HasTile(unwalkableTilemap.WorldToCell(neighbourNode.pos))){
+                    continue;
+                }
+
+            }
+        }
+
+    }
+
+    private Node CreateNewNode(Vector3 pos, Node originNode){
+        Node newNode = new Node();
+
+        if (unwalkableTilemap.HasTile(unwalkableTilemap.WorldToCell(pos))){
+            return newNode;
+        }
+
+        newNode.pos = pos;
+
+        newNode.prevNode  = openNodes.IndexOf(originNode);
+
+        newNode.distance2Start = (pos - transform.position).magnitude;
+
+        newNode.distanceToEnd = pos == transform.position
+            ? 0
+            : (targetPath - transform.position).magnitude;
+
+        newNode.total = newNode.distance2Start + newNode.distanceToEnd;
+
+        return newNode;
+    }
+
+    private Node CreateStartingNode(Vector3 pos){
+        Node newNode = new Node();
+
+        newNode.pos = pos;
+        newNode.prevNode = -1;
+        
+        return newNode;
+    }
+
+    private Node FindLowestDistance2Start(){
+        Node lowestDistance2StartNode = openNodes[0];
+
+        if (openNodes.Count == 1){
+            return lowestDistance2StartNode;
+        }
+
+        foreach (Node node in openNodes){
+            if (node.total < lowestDistance2StartNode.total){
+                lowestDistance2StartNode = node;
+            }
+            else if (node.total == lowestDistance2StartNode.total && node.distance2Start < lowestDistance2StartNode.distance2Start){
+                lowestDistance2StartNode = node;
+            }
+        }
+
+        return lowestDistance2StartNode;
+
+    }
+
+    private Node[] CreateNeighbours(Node originNode){
+        Node[] newOpenNodes = new Node[8];
+        int nodeNumber = 0;
+
+        foreach (int y in surroudingNodesOffset){
+            foreach (int x in surroudingNodesOffset){
+                if (nodeNumber > 8){ break;}
+                CreateNewNode(new Vector3(x,y,0), originNode);
+
+                nodeNumber++;
+            }
+        }
+
+        return newOpenNodes;
+    }
+
+    private float distanceFromStart(Node node){
+        float distanceFromStart = 0;
+
+
+        return distanceFromStart;
+
+    }
+
 }
